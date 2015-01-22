c2614930c677d06e2ce5f326d9736de08f4915d6
aligo Kang <aligo_x@163.com>
2015-01-22 18:43:42 +0800

    test lambda (brainfuck)

--- tests/lambda.m
+++ tests/lambda.m
@@ -12,5 +12,39 @@ int main() {
 
   NSLog(@"(apply, (lambda, [list, 'a'], (+, 1, 1, (sget, 'a'))), 2) should be: 4 == %@", nsMudTestEvaluate(@[@131, @[@130, @[@500, @"a"], @[@200, @1, @1, @[@120, @"a"]]], @2]));
 
+  NSLog(@"(apply,"
+          " (lambda, [list, 'a', 'b, 'c'],"
+            " (+, 1, 1, (sget, 'a'), (sget, 'b'), (sget, 'c'))"
+          " ),"
+        " 2, 2"
+      " ) should be: 6 == %@", nsMudTestEvaluate(@[@131, 
+            @[@130, @[@500, @"a", @"b", @"c"], 
+              @[@200, @1, @1, @[@120, @"a"], @[@120, @"b"], @[@120, @"c"]]
+            ], 
+          @2, @2
+        ]));
+
+    NSLog(@"(apply,"
+          " (lambda, [list, 'arg1', 'arg2'],"
+            " (concat, 'a', (sget, 'arg1'), ( apply,"
+              " ( lambda, [list, 'arg1'],"
+                " (concat, (sget, 'arg1'), 'd')"
+              " )"
+            " , (sget, 'arg1') )"
+            " )"
+          " ),"
+        " 'b', 'c'"
+      " ) should be: abcd == %@", nsMudTestEvaluate(@[@131, 
+            @[@130, @[@500, @"arg1", @"arg2"], 
+              @[@300, @"a", @[@120, @"arg1"], @[@131, 
+                @[@130, @[@500, @"arg1"], 
+                  @[@300, @[@120, @"arg1"], @"d"]
+                ]
+              , @[@120, @"arg2"]]
+              ]
+            ],
+          @"b", @"c"
+        ]));
+
   NSLog(@"ok");
 }
