df19299f8bfc6442863b9ba0730b2839c941bb9d
aligo Kang <aligo_x@163.com>
2015-04-24 16:59:47 +0800

    ARG OR

--- src/helpers/expr_arg.h
+++ src/helpers/expr_arg.h
@@ -8,3 +8,8 @@
 #define ME_ARG_BOOLEAN(I)        mud_object_try_cast_boolean(evaluator->pool, _mud_expr_evaluator_get(evaluator, I))
 #define ME_ARG_INT(I)            mud_object_try_cast_int(evaluator->pool, _mud_expr_evaluator_get(evaluator, I))
 #define ME_ARG_FLOAT(I)          mud_object_try_cast_float(evaluator->pool, _mud_expr_evaluator_get(evaluator, I))
+
+#define ME_ARG_STR_OR(I, OR)            mud_object_try_cast_str_with_default(evaluator->pool, _mud_expr_evaluator_get(evaluator, I), OR)
+#define ME_ARG_BOOLEAN_OR(I, OR)        mud_object_try_cast_boolean_with_default(evaluator->pool, _mud_expr_evaluator_get(evaluator, I), OR)
+#define ME_ARG_INT_OR(I, OR)            mud_object_try_cast_int_with_default(evaluator->pool, _mud_expr_evaluator_get(evaluator, I), OR)
+#define ME_ARG_FLOAT_OR(I, OR)          mud_object_try_cast_float_with_default(evaluator->pool, _mud_expr_evaluator_get(evaluator, I), OR)
