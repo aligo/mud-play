d378b615e8a00af4d6830a49e1a6a8a8ff74a9f3
aligo Kang <aligo_x@163.com>
2016-05-24 19:25:04 +0800

    fix op_list_slice

--- operators/510_list.c
+++ operators/510_list.c
@@ -148,6 +148,9 @@ mud_object_t * _mud_op_list_slice_evaluate(mud_expr_evaluator_t * evaluator) {
     mud_list_t * list = (mud_list_t *)org->ptr;
     mud_int_t start = _mud_list_prepare_index(list, ME_ARG_INT(1));
     mud_int_t end = ( ME_ARGC > 2 ) ? _mud_list_prepare_index(list, ME_ARG_INT(2)) : list->count;
+    if ( end > list->count ) {
+      end = list->count;
+    }
     
     mud_object_t * ret = mud_object_alloc(evaluator->stack, MUD_OBJ_TYPE_LIST);
     ret->ptr = mud_list_alloc();
